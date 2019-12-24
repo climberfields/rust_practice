b0ee78ee4acb82db9082402429c00b6f78e1a98c
Alex <climberfields@gmail.com>
2019-12-24 14:39:25 -0600

    learning about structs

---
+++ structs/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Hello, world!");
+
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+
+    
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+
+        };
+    user1.email = String::from("anotheremail@example.com")
+    }
+}
+
+
+
+fn build_user(email::String, username::String) -> User {
+    User{ 
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+   
+}
